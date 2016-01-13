e5d4f92589ed072dfa091f7e8003616b68623b7a
Tom Hallam <tom@freshnode.co.uk>
2016-01-13 17:03:01 +0000

    Added branch selection in arguments, some other tidying up

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -10,21 +10,21 @@ var version = ebDeploy.version;
 //const devCreds = require('root-require')('./creds.json');
 
 // Set up command line args
-ebArgs.version(version).option('-a, --accessKeyId <key>', 'Set AWS Access Key').option('-s, --secretAccessKey <key>', 'Set AWS Secret Access Key').option('-r, --region', 'Set AWS Region [eu-west-1]', 'eu-west-1').option('-A, --applicationName <name>', 'The name of your Elastic Beanstalk Application [my-app]', 'my-app').option('-e, --environment <name>', 'Which environment should this application be deployed to? [environment]', 'my-environment').option('-b, --bucketName <name>', 'The name of the *existing* S3 bucket to store your version [my-bucket]', 'my-bucket').option('-p, --packageLocation', 'Location of package.json if not ./package.json').option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble').parse(process.argv);
+ebArgs.version(version).option('-a, --accessKeyId <key>', 'Set AWS Access Key').option('-s, --secretAccessKey <key>', 'Set AWS Secret Access Key').option('-r, --region <region>', 'Set AWS Region [eu-west-1]', 'eu-west-1').option('-A, --applicationName <name>', 'The name of your Elastic Beanstalk Application').option('-e, --environment <name>', 'Which environment should this application be deployed to?').option('-b, --bucketName <name>', 'The name of the *existing* S3 bucket to store your version').option('-B, --branch <name>', 'The branch that should be used to generate the archive [master]', 'master')
+//.option('-p, --packageLocation', 'Location of package.json if not ./package.json')
+.parse(process.argv);
 
 // Check arguments
-/*
 // Back out if we've not set the required keys
-if(!ebArgs.accessKeyId) {
+if (!ebArgs.accessKeyId) {
   console.error('AWS Access Key must be set!');
   process.exit(1);
 }
 
-if(!ebArgs.secretAccessKey) {
+if (!ebArgs.secretAccessKey) {
   console.error('AWS Secret Access Key must be set!');
   process.exit(1);
 }
-*/
 
 // All projects require a package.json
 try {
@@ -55,7 +55,7 @@ utils.makeVersionsFolder().then(function () {
   return utils.getGitTag();
 }).then(function (tag) {
   project.version = tag;
-  return utils.createArchive('master', tag);
+  return utils.createArchive(ebArgs.branch, tag);
 }).then(function () {
   return elasticBeanstalk.createVersionAndDeploy({
     environment: ebArgs.environment,
--- src/lib/index.js
+++ src/lib/index.js
@@ -5,23 +5,22 @@ const utils = require('./utils');
 
 const ebDeploy = require('root-require')('./package.json');
 const version = ebDeploy.version;
-//const devCreds = require('root-require')('./creds.json'); 
+//const devCreds = require('root-require')('./creds.json');
 
 // Set up command line args
 ebArgs
   .version(version)
   .option('-a, --accessKeyId <key>', 'Set AWS Access Key')
   .option('-s, --secretAccessKey <key>', 'Set AWS Secret Access Key')
-  .option('-r, --region', 'Set AWS Region [eu-west-1]', 'eu-west-1')
-  .option('-A, --applicationName <name>', 'The name of your Elastic Beanstalk Application [my-app]', 'my-app')
-  .option('-e, --environment <name>', 'Which environment should this application be deployed to? [environment]', 'my-environment')
-  .option('-b, --bucketName <name>', 'The name of the *existing* S3 bucket to store your version [my-bucket]', 'my-bucket')
-  .option('-p, --packageLocation', 'Location of package.json if not ./package.json')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .option('-r, --region <region>', 'Set AWS Region [eu-west-1]', 'eu-west-1')
+  .option('-A, --applicationName <name>', 'The name of your Elastic Beanstalk Application')
+  .option('-e, --environment <name>', 'Which environment should this application be deployed to?')
+  .option('-b, --bucketName <name>', 'The name of the *existing* S3 bucket to store your version')
+  .option('-B, --branch <name>', 'The branch that should be used to generate the archive [master]', 'master')
+  //.option('-p, --packageLocation', 'Location of package.json if not ./package.json')
   .parse(process.argv);
 
 // Check arguments
-/*
 // Back out if we've not set the required keys
 if(!ebArgs.accessKeyId) {
   console.error('AWS Access Key must be set!');
@@ -32,7 +31,7 @@ if(!ebArgs.secretAccessKey) {
   console.error('AWS Secret Access Key must be set!');
   process.exit(1);
 }
-*/
+
 
 // All projects require a package.json
 try {
@@ -41,7 +40,7 @@ try {
 catch(e) {
   console.error('No package.json found, exiting');
   console.error(e);
-  process.exit(1);
+  process.exit(1); 
 }
 
 const project = {
@@ -66,7 +65,7 @@ utils.makeVersionsFolder()
   })
   .then(function(tag) {
     project.version = tag;
-    return utils.createArchive('master', tag)
+    return utils.createArchive(ebArgs.branch, tag)
   })
   .then(function() {
     return elasticBeanstalk.createVersionAndDeploy({
